19426e86b1fcd26ab91fab20f8ed38e1e7a19937
Kevin Ngo <kevinngo2002@gmail.com>
2025-04-11 03:44:01 -0400

    Implement basic S&P 500 predictor with RandomForest model

--- main.py
+++ main.py
@@ -31,4 +31,40 @@ sp500['Tomorrow'] = sp500['Close'].shift(-1)
 
 #Will return 1 if the price went up, 0 if it went down
 sp500["Target"] = (sp500["Tomorrow"] > sp500["Close"]).astype(int)
+
+
+#Remove all data before 1990, this will only select data from 1990 and after
+sp500 = sp500.loc["1990-01-01":].copy()
 print(sp500)
+
+#Training the model
+
+from sklearn.ensemble import RandomForestClassifier #trains individual decision trees with randomized parameters
+
+#creates a Random Forest Classification model with scikit-learn
+#This will build 100 individual ecision trees and require 100 samples to split a node
+model = RandomForestClassifier(n_estimators=100, min_samples_split=100, random_state=1)
+
+#Puts everything except the last 100 rows into the training set
+train = sp500.iloc[:-100]
+
+#Puts the last 100 rows into the test set
+test = sp500.iloc[-100:]
+
+#list all of all the columns we will use to predict
+predictors = ["Close", "Volume", "Open", "High", "Low"]
+model.fit(train[predictors], train["Target"])
+
+
+
+#Imports precison score which will evaluate the model's performance
+from sklearn.metrics import precision_score
+
+#Making predictions on the test set by selecting the features we're using to predict
+#model.predict will return an array of 1s and 0s
+preds = model.predict(test[predictors])
+
+#imports pandas to convert the array into a series
+import pandas as pd
+preds = pd.Series(preds, index=test.index.date)
+print(preds)
